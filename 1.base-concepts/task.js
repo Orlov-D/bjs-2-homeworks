9eab802808f72345e1c032fda0f3946861d339d7
unknown <orlovdimas92@mail.ru>
2021-09-21 08:53:48 +0300

    курсы выровнтлись

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -3,11 +3,10 @@
 function solveEquation(a, b, c) {
   let arr = [];
   let d= b ** 2 - 4 * a *c;
-  if (d < 0) {
-	  arr = [];
-  } else if (d == 0) {
+  if (d == 0) {
 	  arr.push(-b / (2 * a));
-  } else {
+  } 
+  if (d > 0) {
 	  arr.push((-b + Math.sqrt(d)) / (2 * a));
 	  arr.push((-b - Math.sqrt(d)) / (2 * a));  
   }
