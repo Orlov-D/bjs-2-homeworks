402a340f78347d8189ba1801feb8f1490146bd11
unknown <orlovdimas92@mail.ru>
2021-09-29 00:42:31 +0300

    Все тесты работают, а вот вывод некорректен. Возможно и ввод...

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  name: name;
+  gender: gender;
+  age: age;
+  subject: "";
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+    this.marks.push(mark);
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = [];
+  }
+  for (el of mark) {
+    this.marks.push(el);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  if (this.marks === undefined) {
+    return console.error(`Еще не учился`);
+  } else {
+    for (el of this.marks) {
+      sum += el;
+    }
+    return sum / this.marks.length;
+  }
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
